f9d9c990bd173b62ca3324ff24eb2fe485500c4e
Maksim Chertkov <maxch@unl.solutions>
2026-03-24 01:42:13 +0100

    update node according to new API

--- balance.js
+++ balance.js
@@ -1,154 +1,129 @@
-
-const io = require('socket.io-client');
 const fetch = require('node-fetch');
 
+/* response is:
+{
+    "current_page": 1,
+    "data": [
+        {
+            "id": 12345678,
+            "user_id": 12345678,
+            "login": "12345678901234",
+            "btk_id": "1234567890123",
+            "error": null,
+            "line_type": 16,
+            "max_promised_payment_amount": "20.00",
+            "balance": 59.69,
+            "status": "complete",
+            ...
+            "applications": [
+               ...
+            ],
+            "cctv_available": null,
+            "has_delayed_actions": false,
+            "price": 41.8,
+            "terminate_in": 42
+        }
+    ],
+    "first_page_url": "https:\/\/myapi.beltelecom.by\/api\/v2\/contracts?page=1",
+    "from": 1,
+    "next_page_url": null,
+    "path": "https:\/\/myapi.beltelecom.by\/api\/v2\/contracts",
+    "per_page": 9,
+    "prev_page_url": null,
+    "to": 1
+}
+*/
+function getBalance(token, cb) {
 
-function getBalance(login, pass, cb){
+    if (!token) {
+        cb && cb(new Error('No token (not authorized?)'));
+        return
+    }
 
-	fetch("https://myapi.beltelecom.by/api/v1/contracts/check/balance", {
-	  "headers": {
-	    "accept": "application/json",
-	    "accept-language": "en-US,en;q=0.9",
-	    "content-type": "application/json",
-	    "hl": "ru",
-	    "sec-fetch-dest": "empty",
-	    "sec-fetch-mode": "cors",
-	    "sec-fetch-site": "same-site",
-	    "x-client": "web"
-	  },
-	  "referrer": "https://my.beltelecom.by/check-balance",
-	  "referrerPolicy": "no-referrer-when-downgrade",
-	  "body": `{\"login\": \"${login}\",\"password\": \"${pass}\"}`,
-	  "method": "POST",
-	  "mode": "cors"
-	}).then(r => r.json()).then(r =>{
-		this.log('TOKEN RECEIVED', r);
+    fetch("https://myapi.beltelecom.by/api/v2/contracts", {
+        "headers": {
+            'authorization': 'Bearer ' + token,
+        },
+        "referrer": "https://my.beltelecom.by",
+        "method": "GET"
+    }).then(r => r.json()).then(data => {
 
-		var socket = io('https://myws.beltelecom.by', {
-			// namespace: "App.Events",
-		});
-		socket.on('connect', (client) => {
-			this.log('byfly: connected' );
-			this.log('byfly: subscribe to topic', r);
-			
-			socket.emit("subscribe",{"channel":r.channel,"auth":{"headers":{"Authorization":"Bearer undefined"}}});
-		});
-		socket.on('disconnect', () => {
-			this.log('byfly: disconnected' );
-		});
+        this.log('DATA RECEIVED', data);
 
+        let balance = null;
+        try {
+            balance = data.data[0].balance;
+        } catch (e) {
+            this.error('byfly: structure error:', e);
+            cb && cb(e);
+        }
 
-		socket.on('App\\Events\\PublicPush', (msgid, data) => {
-			this.log('byfly: incoming', data);
-			socket.emit("unsubscribe",{"channel":r.channel});
-			socket.disconnect();
+        cb && cb(null, balance, data);
 
-			// in timeout, so we close socket as soon as we can
-			setTimeout(()=>{
-
-				/*
-
-				Data is: 
-				{
-					balance: 7.57
-					initiator: "flow"
-					login: "176XXXXXXXXXX"
-					message: "176XXXXXXXXXX"
-					services: ["Доступ к сети интернет", "Информирование о приближении к порогу отключения"]
-					socket: null
-					status: "success"
-					subject: "Баланс"
-					tariff: "Комфорт Экспресс"
-					type: "App\IntegrationBus\Flows\CheckLogin"
-					uuid: "df398102-ed0f-435e-af56-9e4a1725b0f0"
-
-				}
-
-				Error is: {
-					initiator: 'flow',
-					type: 'App\\IntegrationBus\\Flows\\CheckLogin',
-					status: 'error',
-					error: {
-						code: 'issa.GetAuth.code_318',
-						message: 'Неверный логин или пароль'
-					},
-					subject: 'Произошла ошибка',
-					message: 'Неверный логин или пароль',
-					socket: null
-				}
-				*/
-				
-				if(!data){
-					cb && cb(new Error('Empty response'));
-					return;
-				}
-				
-				if(data.error){
-					cb && cb(new Error(data.message || 'Unknown error format. Plugin needs an update =('));
-					return;
-				}
-				
-				if(typeof data.balance === 'undefined'){
-					cb && cb(new Error('No balance in response'));
-					return;
-				}
-
-				cb && cb(null, data ? data.balance : null, data);
-			});
-		});
-
-	}).catch(e =>{
-		this.error('byfly: error:', e);
-		cb && cb(e);
-	});
+    }).catch(e => {
+        this.error('byfly: request error:', e);
+        cb && cb(e);
+    });
 }
 
 /**
  *
- * 
- * 
+ *
+ *
  */
-module.exports = function(RED) {
+module.exports = function (RED) {
     function ByFlyBalanceNode(config) {
         RED.nodes.createNode(this, config);
-		var node = this;
-		
-		this.status({}); // clear status on deploy
+        var node = this;
 
-        node.on('input', function(msg, send, done) {
-			
-			var username = this.credentials.username;
-			var password = this.credentials.password;
+        // node started
+        var token = node.credentials.token;
+        if (token) {
+            // TODO: set timer to resfresh token (no refresh endpoint?)
+            this.log('TOKEN:' + token.substr(0, 6));
+        }
 
-			this.status({fill: "yellow", shape: "dot", text: "fetching"});
 
-			getBalance.call(node, username, password, (err, balance, _raw)=>{
+        this.status({}); // clear status on deploy
 
-				if(err){
-					this.status({fill: "red", shape: "ring", text: "error"});
+        this.on('close', function () {
+            // TODO: clear timer to resfresh token
+        });
 
-					// https://nodered.org/docs/creating-nodes/node-js#handling-errors
-					if (done) {
-						// Node-RED 1.0 compatible
-						done(err);
-					} else {
-						// Node-RED 0.x compatible
-						node.error(err, msg);
-					}
-				} else { 
-					this.status({}); // clear status
+        node.on('input', function (msg, send, done) {
+            var token = node.credentials.token;
 
-					msg.payload = parseFloat(balance);
-					msg.raw = _raw;
-					node.send(msg);
-				}
-			});
+            this.status({fill: "yellow", shape: "dot", text: "fetching"});
+
+            getBalance.call(node, token, (err, balance, _raw) => {
+
+                if (err) {
+                    node.status({fill: "red", shape: "ring", text: "error"});
+
+                    // https://nodered.org/docs/creating-nodes/node-js#handling-errors
+                    if (done) {
+                        // Node-RED 1.0 compatible
+                        done(err);
+                    } else {
+                        // Node-RED 0.x compatible
+                        node.error(err, msg);
+                    }
+                } else {
+                    node.status({}); // clear status
+
+                    msg.payload = parseFloat(balance);
+                    msg.raw = _raw;
+                    node.send(msg);
+                }
+            });
         });
     }
-    RED.nodes.registerType("byfly-balance",ByFlyBalanceNode, {
+
+    RED.nodes.registerType("byfly-balance", ByFlyBalanceNode, {
         credentials: {
-            username: {type:"text"},
-            password: {type:"password"}
+            username: {type: "text"},
+            password: {type: "text"},
+            token: {type: "password"}
         },
-	});
+    });
 }
